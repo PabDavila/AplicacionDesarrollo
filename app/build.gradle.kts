1824050cce941dfb92047dbc53cf22a4e2b8bc72
PabDavila <pab.davila@duocuc.cl>
2025-09-24 13:34:49 -0300

    Hotfixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,5 +74,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.1")
 
 }
